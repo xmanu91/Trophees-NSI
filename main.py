97215b6040e904cbf4985ad643221f153338bf8a
Emmanuel LINGUET <xmanu.linguet@gmail.com>
2024-12-19 14:54:45 +0100

    Pygame initialisation

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+import sys
+
+pygame.init()
+
+WIDTH, HEIGHT = 900, 500
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.quit()
+    
+    pygame.display.flip()
