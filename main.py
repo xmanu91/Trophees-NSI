494bd070c09c38fe14b1e43d56619ec613530f01
Emmanuel LINGUET <xmanu.linguet@gmail.com>
2024-12-21 12:28:36 +0100

    Some errors fixed

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.quit()
+            sys.exit()
     
     pygame.display.flip()
