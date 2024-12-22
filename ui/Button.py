188b359067db058920fec3f7c7e4d6ff5731a4c9
malo-0 <benagriamalo@gmail.com>
2024-12-22 19:05:23 +0100

    Update Button.py

--- ui/Button.py
+++ ui/Button.py
@@ -1,48 +1,33 @@
 import pygame
 
 class Button(): 
-    def __init__(self, default_color, hover_color, width, height, x, y, action, image, x_image, y_image, text, x_text, y_text):
-
+    def __init__(self, default_color, hover_color, buttonRect, action, image, imageCoordinates, text, textCoordinates):
         self.default_color = default_color  
         self.hover_color = hover_color  
-        self.width = width 
-        self.height = height 
-        self.x = x
-        self.y = y
+        self.buttonRect = pygame.Rect(buttonRect)
         self.action = action
-        #image plus sa position :
-        self.image = pygame.image.load("image.png").convert_alpha() 
-        self.x_image = x_image
-        self.y_image = y_image
+
+        # image + sa position :
+        self.image = pygame.image.load(image).convert_alpha() # j'ai mis "convert_alpha() pour gérer une potentiel transparence"
+        self.imageCoordinates = pygame.Rect(imageCoordinates, self.image.get_size()) # imageCoordinates = x,y et self.image.get_size() = width, height
+
         # texte + sa position :
-        self.text = text 
-        self.x_text = x_text
-        self.y_text = y_text
-
-
-        self.surface = pygame.Surface((self.width, self.height)) #Surface du bouton
-        self.position = pygame.Rect((self.x, self.y, self.width, self.height))  #Position du bouton sur l'écran
-        self.position_image = self.image.get_rect(topleft=(self.x_image, self.y_image)) #Position de l'image
-
         self.font = pygame.font.Font(None, 36)  # police par défaut, taille 36 pixels
-        self.surface_text = self.font.render(self.text, True, (0, 0, 0))  # texte en noir
-        self.position_text = self.surface_text.get_rect(topleft=(self.x_text, self.y_text)) # position du texte
-        
+        self.surface_text = self.font.render(text, True, (0, 0, 0))  # texte en noir
+        self.textCoordinates = pygame.Rect(textCoordinates, self.surface_text.get_size()) 
 
+        # surface du bouton :
+        self.surface = pygame.Surface(self.buttonRect.size) # self.buttonRect.size = (width, height)
 
-    def update(self): #la fonction qui sera mise à jour en boucle à chaque frame
-        pos = pygame.mouse.get_pos() #prend la position du curseur
-        mouse_pressed = pygame.mouse.get_pressed()[0] # vérifie si le bouton gauche de la souris est pressé
+    def update(self):  # la fonction qui sera mise à jour en boucle à chaque instant
+        pos = pygame.mouse.get_pos()  # prend la position du curseur
+        mouse_pressed = pygame.mouse.get_pressed()[0]  # vérifie si le bouton gauche de la souris est pressé
 
-        if self.position.collidepoint(pos): #si curseur touche un bouton
-            self.surface.fill(self.hover_color) 
-            if mouse_pressed :  
-                self.surface.fill((255,0,0))
- # on changera cette couleur quand on verra la palette de couleur qu'on utilisera sur l'écran 
-
+        if self.buttonRect.collidepoint(pos):  # si le curseur touche un bouton
+            self.surface.fill(self.hover_color)
+            if mouse_pressed:  
+                self.surface.fill((255, 0, 0))  # couleur du clic (rouge, on changera la couleur je pense)
                 if self.action:
-                    self.action() 
-
-        else :  #s'execute si le curseur ne touche pas/plus le bouton
-            self.surface.fill(self.default_color) 
-
+                    self.action()
+        else:  # s'exécute si le curseur ne touche pas/plus le bouton
+            self.surface.fill(self.default_color)
