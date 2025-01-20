bb1d424240990064f305eece7e447c5b19fe2ce2
malo-0 <benagriamalo@gmail.com>
2025-01-20 14:46:14 +0100

    fix: scene manager

--- ui/SceneManager.py
+++ ui/SceneManager.py
@@ -1,11 +1,19 @@
-from Scene import Scene
+from ui.Scene import Scene
+import pygame
 
 class SceneManager:
     def __init__(self, surface):
-        self.surface = surface
-        self.currentScene = None
+        self.surface: pygame.Surface = surface
+        self.currentScene: Scene = None
 
     def setAsCurrentScene(self, scene: type[Scene]) -> None:
         if self.currentScene != None:
-            self.currentScene.empty()
-        scene.draw(self.surface)
+            pass
+        self.currentScene = scene
+        self.draw()
+
+    def draw(self):
+        self.currentScene.spriteGroup.draw(self.surface)
+
+    def update(self):
+        self.currentScene.spriteGroup.update()
