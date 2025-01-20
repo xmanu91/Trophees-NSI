2aa484d4d644fadbb7955f93d1f299e2ba132bd9
malo-0 <benagriamalo@gmail.com>
2025-01-20 14:45:58 +0100

    feat: Image sprite

---
+++ ui/Image.py
@@ -0,0 +1,9 @@
+import pygame
+import os
+
+class Image(pygame.sprite.Sprite):
+    def __init__(self, image: str, imageCoordinates: pygame.Rect):
+        super().__init__()
+        self.image = pygame.image.load(os.path.join(image))
+        self.image = pygame.transform.scale(self.image, imageCoordinates.size).convert_alpha()
+        self.rect = imageCoordinates
