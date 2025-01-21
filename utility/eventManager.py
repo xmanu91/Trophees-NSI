d7fd5369058df89a41e62d418646fdcc1d74ff9e
Emmanuel LINGUET <xmanu.linguet@gmail.com>
2025-01-21 14:20:11 +0100

    fix: eventManager

--- utility/eventManager.py
+++ utility/eventManager.py
@@ -1,13 +1,16 @@
 import pygame
 
-eventHandlers = {pygame.QUIT: pygame.quit}
+eventHandlers = {pygame.QUIT: [pygame.quit]}
 
-    
-    
 def addEventHandler(event, action):
-    eventHandlers[event] = action
+    if event not in eventHandlers:
+        eventHandlers[event] = [action]
+    else:
+        eventHandlers[event].append(action)
     
 def update(event):
-    for handler in eventHandlers:
-        if event.type == handler:
-            eventHandlers[handler](event)
+    for handledEvent in eventHandlers:
+        if event.type == handledEvent:
+            for i in range(len(eventHandlers[handledEvent])):
+                eventHandlers[handledEvent][i](event)
+                
