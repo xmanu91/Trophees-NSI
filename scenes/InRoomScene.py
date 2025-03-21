e0fb07ff1a9f3e10299e7340544869adac2040ef
Thzgeek11 <65951876+Thzgeek11@users.noreply.github.com>
2025-03-21 11:16:55 +0100

    Fix sync (#49)
    * Starting Fix
    
    * T1
    
    * T2
    
    * T3
    
    * Final fix
    
    Le visuel de la souris change en un rond bleu pour indiquer la tentative de récuperation des votes
    
    * Delete Inkspired v1.1.S5 - Dev Version.exe
    
    * Update VoteScene.py
    
    * Update WinnerScene.py
    
    ---------
    
    Co-authored-by: Emmanuel LINGUET <65608769+xmanu91@users.noreply.github.com>

--- scenes/InRoomScene.py
+++ scenes/InRoomScene.py
@@ -58,6 +58,8 @@ class InRoomScene(Scene):
         self.updateStateEventType = pygame.event.custom_type()
         pygame.time.set_timer(pygame.event.Event(self.updateStateEventType), 1000)
         eventManager.addEventHandler(self.updateStateEventType, self.checkGameState)
+
+        self.gameLaunched = False
         
     def updateConnectedUsers(self, e=None): # e parameters is due to eventHandler contraints
         consolLog.info('Actualisation...')
@@ -82,8 +84,15 @@ class InRoomScene(Scene):
         print(self.connectedUsers)
 
     def startGame(self):
+        if self.gameLaunched == True:
+            consolLog.warn("La partie est deja en cours")
+            return
+        else:
+            self.gameLaunched = True
+
         self.dev = True
-        self.playButton.disable()
+        
+        consolLog.warn("Debut de la partie...")
         if len(self.connectedUsers) < 2 and self.dev == False:
             raiseAnError("Vous devez être plusieurs pour pouvoir jouer")
             self.updateConnectedUsers()
@@ -91,11 +100,11 @@ class InRoomScene(Scene):
             if self.isUserRoomCreator:
                 self.roomManager.setRoomState('playing')
             else:
-                time.sleep(2)
+                time.sleep(0.75)
             pygame.time.set_timer(pygame.event.Event(self.updateStateEventType), 0)
             paintingScene = scenes.PaintingScene.PaintingScene(self.sceneManager, self.roomManager, self.gameManager)
             self.sceneManager.setAsCurrentScene(paintingScene)
-            
+        
     def quitGame(self):
         consolLog.info("Room quitter")
         if self.isUserRoomCreator:
